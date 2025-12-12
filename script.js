28761e48a7a8f2809f2a0caa3e4a3fd1317032bd
Arnav <92578817+arnav1256@users.noreply.github.com>
2025-12-12 09:57:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -39,10 +39,10 @@ function loadCoreStaffCarousel() {
             slideItems += `
                 <div class="col-auto text-center p-0">
                     <div class="position-relative d-inline-block">
-                        <img src=${img} style="width:240px; height:240px;">
+                        <img class="curve" src=${img} style="width:240px; height:240px;">
                         <div class="position-absolute top-0 start-0 w-100 h-100 d-flex 
                                     align-items-center justify-content-center bg-opacity-50
-                                    text-black fw-bold fs-5 hover-overlay">
+                                    text-black fw-bold fs-5 hover-overlay curve">
                             ${person.name}
                         </div>
                     </div>
@@ -87,7 +87,7 @@ function loadConventionsForLibrary() {
 
             slidesHTML += `
                 <div class="carousel-item ${i === 0 ? "active" : ""}">
-                    <div class="row justify-content-center gap-3">
+                    <div class="row justify-content-center gap-3 button-group">
                         ${slideItems}
                     </div>
                 </div>
